56b2c011e1979227f66643b03b2aba3e60ffb193
gustavoclay <gustavoclay@gmail.com>
2025-08-20 21:29:41 -0300

    Criando primeiro projeto NODE

---
+++ praticas/01-primeiro-projeto/index.js
@@ -0,0 +1,21 @@
+console.log("Rodando meu primeiro projeto em NODE")
+
+let nome = "Joãozinho"
+
+let sobreNome = "Clay"
+
+console.log(nome + " " + sobreNome)
+
+let idade = 35
+
+console.log("A idade do " + nome + " é " + idade)
+
+let exercicios = 0.5
+
+let trabalho = 1.5
+
+let prova = 6
+
+let notaA1Calculada = exercicios + trabalho + prova
+
+console.log("NOTA A1: " + notaA1Calculada)
