cc5036adfbe97e6ef017a2a2ee38cf79666431d3
gustavoclay <gustavoclay@gmail.com>
2025-09-18 21:26:49 -0300

    projeto CRUD completo

--- praticas/06-crud-pessoas/routes/Pessoas.js
+++ praticas/06-crud-pessoas/routes/Pessoas.js
@@ -33,7 +33,7 @@ router.get('/pessoas/:id', (req, res, next) => {
   const id = req.params.id
   // faço a busca na lista de pessoas pelo id recebido
   const pessoa = listaPessoas.find(pessoa => pessoa.id == id)
-  if(!pessoa) {
+  if (!pessoa) {
     return res.status(404).json({ error: "Pessoa não encontrada!!!" })
   }
   res.json(pessoa)
@@ -41,17 +41,65 @@ router.get('/pessoas/:id', (req, res, next) => {
 
 // #Criação
 // POST /pessoas
-// req.body
-// new Date()
+router.post('/pessoas', (req, res, next) => {
+  const { nome, cpf, email, dataNascimento } = req.body
+  // Validando se todos os campos foram preenchidos
+  if (!nome || !cpf || !email || !dataNascimento) {
+    return res.status(400).json({ error: "Nome, cpf, email e DataNascimento são obrigatórios!!!" })
+  }
 
+  // validar se o cpf já foi cadastrado
+  if (listaPessoas.some(pessoa => pessoa.cpf == cpf)) {
+    return res.status(409).json({ error: "CPF já cadastrado!!!" })
+  }
 
+  const novaPessoa = {
+    id: Date.now(),
+    nome,
+    cpf,
+    email,
+    dataNascimento
+  }
+
+  listaPessoas.push(novaPessoa)
+  res.status(201).json({ message: "Pessoa cadastrada com sucesso", novaPessoa })
+})
 
 // #Atualização
-// PUT ou PATCH /pessoas/:id
-// req.body
+// PUT /pessoas/:id
+router.put('/pessoas/:id', (req, res, next) => {
+  const id = req.params.id
+  const pessoa = listaPessoas.find(pessoa => pessoa.id == id)
+  // valido se a pessoa existe
+  if (!pessoa) {
+    return res.status(404).json({ error: "Pessoa não encontrada!!!" })
+  }
+  // validando se os dados pra atualizar vinheram na requisição
+  const { nome, email, dataNascimento } = req.body
+  if (!nome || !email || !dataNascimento) {
+    return res.status(400).json({ error: "nome, email, dataNascimento são obrigatórios!!!" })
+  }
+  // atualizo os dados da pessoa
+  pessoa.nome = nome
+  pessoa.email = email
+  pessoa.dataNascimento = dataNascimento
+  // responde com os dados da pessoa atualizados 
+  res.json({ message: "Pessoa atualizada com sucesso!!!", pessoa })
+})
 
 // #Remoção
 // DELETE /pessoas/:id
+router.delete('/pessoas/:id', (req, res, next) => {
+  const id = req.params.id
+  // validar se a pessoa não existe
+  const pessoa = listaPessoas.find(pessoa => pessoa.id == id)
+  if (!pessoa) {
+    return res.status(404).json({ error: "pessoa não encontrada!!!"})
+  }
+
+  listaPessoas = listaPessoas.filter(pessoa => pessoa.id != id)
+  res.json({ message: "Pessoa excluida com sucesso!!!"})
+})
 
 
 // exportar o roteador
