c0032f551861a0982fffe2ef6ec76b9e6f320ed1
gustavoclay <gustavoclay@gmail.com>
2025-10-15 21:30:08 -0300

    CRUD de taferas como mongoDB

--- praticas/07-crud-tarefas/index.js
+++ praticas/07-crud-tarefas/index.js
@@ -19,9 +19,59 @@ mongoose.connect(url)
     console.log("Conectado ao MongoDB")
   })
   .catch(err => {
-    console.log("Erro ao conectar no banco Mo'ngoDB: ", err)
+    console.log("Erro ao conectar no banco MongoDB: ", err)
   })
 
+// Interface com o banco de dados - Model
+// Cada model(Modelo) representa uma Collection(Tabela)
+const TarefaModel = mongoose.model('Tarefas', new mongoose.Schema(
+  {
+    nome: String
+  }
+))
+
+// CRUD
+
+// Create
+app.post('/tarefas', async (req, res, next) => {
+  const tarefa = req.body
+  if (!tarefa.nome) {
+    return res.status(400).json({ erro: "O campo nome é obrigatório!!!" })
+  }
+  const tarefaCriada = await TarefaModel.create(tarefa)
+  res.status(201).json(tarefaCriada)
+})
+
+// READ
+app.get('/tarefas', async (req, res, next) => {
+  const tarefas = await TarefaModel.find()
+  res.json(tarefas)
+})
+
+// UPDATE
+app.put('/tarefas/:id', async (req, res, next) => {
+  const id = req.params.id
+  const tarefa = req.body
+  if (!tarefa.nome) {
+    return res.status(400).json({ erro: "O campo nome é obrigatório!!!" })
+  }
+
+  const tarefaAtualizada = await TarefaModel.findByIdAndUpdate(id, tarefa, { new: true })
+
+  if (!tarefaAtualizada) {
+    return res.status(404).json({ erro: "Tarefa não encontrada!!!" })
+  }
+  res.json(tarefaAtualizada)
+})
+
+// DELETE
+app.delete('/tarefas/:id', async (req, res, next) => {
+  const id = req.params.id
+  await TarefaModel.findByIdAndDelete(id)
+  res.json({ mensagem: "Tarefa excluida!!!" })
+})
+
+// start
 app.listen(3000, () => {
   console.log("Aplicação rodando em http://localhost:3000")
 })
