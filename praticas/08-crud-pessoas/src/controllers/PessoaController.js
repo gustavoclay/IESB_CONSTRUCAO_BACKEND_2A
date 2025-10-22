88d004f275f12d803828314d8d3c008d649ea36e
gustavoclay <gustavoclay@gmail.com>
2025-10-22 20:32:16 -0300

    iniciando projeto 08-crud-pessoas

---
+++ praticas/08-crud-pessoas/src/controllers/PessoaController.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const PessoaModel = require('../models/PessoaModel')
+
+// Rotas do CRUD
+// Create
+
+// Read
+
+// Update
+
+// Delete
+
+
+
+
+
+
+module.exports = router
