1d2ab0d64ef6f6e7904d16ff4881c4edee71088c
gustavoclay <gustavoclay@gmail.com>
2025-10-23 20:30:22 -0300

    feito o crud, fazer validadores

--- praticas/08-crud-pessoas/src/controllers/PessoaController.js
+++ praticas/08-crud-pessoas/src/controllers/PessoaController.js
@@ -4,15 +4,44 @@ const PessoaModel = require('../models/PessoaModel')
 
 // Rotas do CRUD
 // Create
+router.post('/pessoas', async (req, res, next) => {
+  const pessoa = req.body
+  const pessoaCadastrada = await PessoaModel.create(pessoa)
+  res.status(201).json(pessoaCadastrada)
+})
 
 // Read
+router.get('/pessoas', async (req, res, next) => {
+  const pessoas = await PessoaModel.find()
+  res.json(pessoas)
+})
+
+router.get('/pessoas/:id', async (req, res, next) => {
+  const id = req.params.id
+  const pessoaEncontrata = await PessoaModel.findById(id)
+  if (!pessoaEncontrata) {
+    return res.status(404).json({ erro: "Pessoa não encontrata!!!" })
+  }
+  res.json(pessoaEncontrata)
+})
 
 // Update
+router.put('/pessoas/:id', async (req, res, next) => {
+  const id = req.params.id
+  const novoDados = req.body
+  const pessoaAtualizada = await PessoaModel.findByIdAndUpdate(id, novoDados, { new: true })
+  if (!pessoaAtualizada) {
+    return res.status(404).json({ erro: "Pessoa não econtrada" })
+  }
+  res.json(pessoaAtualizada)
+})
 
 // Delete
-
-
-
+router.delete('/pessoas/:id', async (req, res, next) => {
+  const id = req.params.id
+  await PessoaModel.findByIdAndDelete(id)
+  res.status(204).send()
+})
 
 
 
---
+++ praticas/08-crud-pessoas/src/validators/PessoaValidator.js
@@ -0,0 +1,17 @@
+const yup = require('yup')
+
+// Esquema de validação
+
+
+
+
+
+// Middlewares de validação
+
+
+
+
+// exportar os middlewares
+module.exports = {
+  
+}
